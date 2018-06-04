0daf5e40b656aeeac5d4d818d26b71eb1d0fa785
Rafael sanchez <management@bionic.it>
2018-06-04 17:22:30 -0400

    Add new item functionality complete & data persisted using User Defaults Method.

--- Todoey2/TodoListViewController.swift
+++ Todoey2/TodoListViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Find Mike","Buy Eggos","Destroy Demogorgon"]
+    var itemArray = ["Find Mike","Buy Eggos","Destroy Demogorgon"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        if let items  = defaults.array(forKey: "TodoListArray") as? [String ] {
+            itemArray = items
+        }
+ 
     }
 
     //MARK: - Tableview Datasource Methods
@@ -42,7 +48,36 @@ class TodoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    //MARK: - Add New Items
     
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user hits the add item  button on our uialert
+            self.itemArray.append(textField.text!)
+           
+            self.defaults.set( self.itemArray, forKey: "TodoListArray")
+            self.tableView.reloadData()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+           
+            print(alertTextField.text)
+           // itemArray.append(alertTextField.text)
+        }
+        alert.addAction(action)
+        
+        
+        
+    present(alert, animated: true, completion: nil)
+    }
     
 
 }
